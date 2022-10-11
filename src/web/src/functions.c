8a17feb97a0de243b0508174a6809610beaa1ce1
Artyom Davydov <x43872@gmail.com>
2022-10-11 17:57:25 +0300

    Web output fixes

--- src/web/src/functions.c
+++ src/web/src/functions.c
@@ -3,9 +3,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <malloc.h>
+#include <string.h>
 
 //@clion_formatter:off
-EM_JS(void, js_print, (const char* line), { out(UTF8ToString(line)); });
+EM_JS(void, js_print, (const char* line), {
+    output = UTF8ToString(line);
+    output = output.replace("\u000B", "");
+    output = output.replace("\u0002", "");
+    output = output.replace("                  P", "                  ");
+    if (output == "\n\n\n\n") {
+        output = "\n\n\n"
+    } else if (output == "\n\n\n") {
+        output = "\n\n"
+    } else if (output == "\n\n") {
+        output = "\n"
+    };
+    out(output);
+});
 //@clion_formatter:on
 
 void PRINT(const char* format, ...) {
@@ -14,6 +28,7 @@ void PRINT(const char* format, ...) {
 
     char* print_buffer = malloc(512);
     vsprintf(print_buffer, format, args);
+    strtok(print_buffer, "\n");
 
     va_end(args);
 
